502bb9a7c53dff59dd8021bd64854975d13abd2b
ashwinij28 <ashwinisjagtap10@gmail.com>
2022-03-15 13:54:00 +0530

    updated branch

--- src/Employee/EmployeeWage.java
+++ src/Employee/EmployeeWage.java
@@ -1,12 +1,18 @@
 package Employee;
 
 public class EmployeeWage {
+    double standardWageRate = 20;
+    int standardHours = 8;
+    double totalWage;
     public void calculateAttendance(){
         int IS_FULL_TIME = 1;
         double empcheck = Math.floor(Math.random() *10) % 2;
-        if (empcheck == IS_FULL_TIME)
-            System.out.println("Employee is present");
+        if (empcheck == IS_FULL_TIME) {
+            totalWage = (standardWageRate * standardHours);
+            System.out.println("Employee is present & wages=" + totalWage);
+        }
+
         else
-            System.out.println("Employee is absent");
+            System.out.println("Employee is absent & wages is" + totalWage);
     }
 }
